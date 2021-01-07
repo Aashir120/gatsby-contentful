292e083f3e722f5f4945ad4208755129e6800535
Aashir120 <60323401+Aashir120@users.noreply.github.com>
2021-01-07 21:34:05 +0500

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,12 +2,11 @@ import React from 'react';
 import { Link } from 'gatsby';
 import styles from './footer.module.css';
 export const Footer = () => {
-    return ( <
-        div className = { styles.footer } >
-        <
-        h1 >
-        Explorer | created By Syed Aashir Majeed <
-        /h1>  <
-        /div>
+    return (
+        <div className={styles.footer} >
+        <h1 >
+            Explorer | created By Syed Aashir Majeed 
+            </h1> 
+            </div>
     )
 }
