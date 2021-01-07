863d05458c8f71b876a47da3cd8e9ea0048222c6
Syed Aashir Majeed <syedaashir120@gmail.com>
2021-01-07 16:07:59 +0500

    node

--- gatsby-node.js
+++ gatsby-node.js
@@ -16,7 +16,7 @@ exports.createPages = async({ graphql, actions }) => {
     response.data.allContentfulBlogPost.edges.forEach(edge => {
         createPage({
             path: `/blog/${edge.node.slug}`,
-            component: path.resolve("./src/templates/blog-post.js"),
+            component: path.resolve("./src/templates/blog-post.tsx"),
             context: {
                 slug: edge.node.slug,
             },
